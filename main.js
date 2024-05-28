1ce31dcae64cb0b87a2a46dc6c7f61ea46670ba0
Arman4102 <125773551+Arman4102@users.noreply.github.com>
2024-05-28 17:25:21 +0700

    Web Gform
    Nyoba sir

---
+++ main.js
@@ -0,0 +1,7 @@
+let link = document.querySelector(".v-mobile")
+
+
+link.addEventListener("click", function(event) {
+    event.preventDefault(); // Mencegah aksi default dari link (jika ada)
+    window.location.href = "https://prettyform.addxt.com/a/form/?vf=1FAIpQLSdlG-L2lmhyyUHx_lue1rUMrmEuSGtL3teSxmSCfuFC28YpgA"; // Ganti dengan URL yang Anda inginkan
+});
